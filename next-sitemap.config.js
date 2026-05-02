9817d22d6c2f644663d059f83df5c69705a0c13e
rebelliousi <learneryolo3@gmail.com>
2026-05-02 20:28:00 +0500

    fix: remove ghost nextauth references

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.NEXTAUTH_URL || 'https://alphaeye.vercel.app',
+  siteUrl: 'https://alphaeye.vercel.app',
   generateRobotsTxt: true,
   generateIndexSitemap: false,
   sitemapSize: 7000,
